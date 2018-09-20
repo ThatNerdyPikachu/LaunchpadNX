cf9cb7fc6cb6c84edab5803402ae8f1991ede6c9
Pika <panic@thedis.co>
2018-09-20 16:03:13 -0400

    update build script for acff69a9

--- build.bat
+++ build.bat
@@ -10,28 +10,28 @@ mkdir release
 set GOOS=windows
 set GOARCH=386
 go build -o launchpad-windows-i386.exe
-zip -r9 launchpad-windows-i386.zip fake_tickets hbl.patch launchpad-windows-i386.exe
+zip -r9 launchpad-windows-i386.zip fake_tickets launchpad-windows-i386.exe
 mv launchpad-windows-i386.zip release/launchpad-windows-i386.zip
 del launchpad-windows-i386.exe
 
 set GOOS=windows
 set GOARCH=amd64
 go build -o launchpad-windows-amd64.exe
-zip -r9 launchpad-windows-amd64.zip fake_tickets hbl.patch launchpad-windows-amd64.exe
+zip -r9 launchpad-windows-amd64.zip fake_tickets launchpad-windows-amd64.exe
 mv launchpad-windows-amd64.zip release/launchpad-windows-amd64.zip
 del launchpad-windows-amd64.exe
 
 set GOOS=linux
 set GOARCH=386
 go build -o launchpad-linux-i386.exe
-zip -r9 launchpad-linux-i386.zip fake_tickets hbl.patch launchpad-linux-i386.exe
+zip -r9 launchpad-linux-i386.zip fake_tickets launchpad-linux-i386.exe
 mv launchpad-linux-i386.zip release/launchpad-linux-i386.zip
 del launchpad-linux-i386.exe
 
 set GOOS=linux
 set GOARCH=amd64
 go build -o launchpad-linux-amd64.exe
-zip -r9 launchpad-linux-amd64.zip fake_tickets hbl.patch launchpad-linux-amd64.exe
+zip -r9 launchpad-linux-amd64.zip fake_tickets launchpad-linux-amd64.exe
 mv launchpad-linux-amd64.zip release/launchpad-linux-amd64.zip
 del launchpad-linux-amd64.exe
 
